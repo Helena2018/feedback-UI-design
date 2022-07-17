cb2d1d415b9781f3d218454f498a874c447c2e13
Helena2018 <helena2020liu@gmail.com>
2022-07-17 18:19:44 +1200

    send selected

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
 const ratings = document.querySelectorAll('.rating')
+const ratingContainer = document.querySelector('.ratings-container')
 const panel = document.getElementById('panel')
 const sentBtn = document.getElementById('send')
 
 let selectedRating = 'Satisfied'
-panel.addEventListener('click', (e) => {
+ratingContainer.addEventListener('click', (e) => {
   if(e.target.parentNode.classList.contains('rating')) {
     removeActive()
     e.target.parentNode.classList.add('active')
     selectedRating = e.target.nextElementSibling.innerHTML
-    console.log(selectedRating)
   }
+})
+
+sentBtn.addEventListener('click', (e) => {
+  panel.innerHTML = `
+    <i class="fas fa-heart"></i>
+    <strong>Thank you!</strong>
+    <br>
+    <strong>Feedback: ${selectedRating}</strong>
+    <p>We'll use your feedback to improve our customer support!</p>
+  `
 })
 
 function removeActive() {
