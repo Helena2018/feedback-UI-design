f2799de669bd2b08aaf8ce5a8b2f4f4d2bce77c3
Helena2018 <helena2020liu@gmail.com>
2022-07-17 18:13:19 +1200

    select rating to send

--- script.js
+++ script.js
@@ -2,10 +2,13 @@ const ratings = document.querySelectorAll('.rating')
 const panel = document.getElementById('panel')
 const sentBtn = document.getElementById('send')
 
+let selectedRating = 'Satisfied'
 panel.addEventListener('click', (e) => {
   if(e.target.parentNode.classList.contains('rating')) {
     removeActive()
     e.target.parentNode.classList.add('active')
+    selectedRating = e.target.nextElementSibling.innerHTML
+    console.log(selectedRating)
   }
 })
 
